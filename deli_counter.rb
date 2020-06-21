f21339388d1ef3b85b94b964e15eb88fad8200f2
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-21 15:58:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,7 @@ array.each.with_index(1) do |value, i|
   puts current_line
 end
 
-def take_a_number(array, name)
-  
+def take_a_number(line, new_person)
+  line.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
