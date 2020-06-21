062e7eec8e888647e1bdb7dac705eb9618d64fe2
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-21 15:56:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,9 @@ katz_deli = []
 def line(array)
 puts "The line is currently empty." if array.empty?
 current_line ='The line is currently:' 
-array.each do |i|
-puts " #{counter}. #{i}"
-counter += 1 
-end
+array.each.with_index(1) do |value, i|  
+    current_line << " #{indexemus}. #{value},"    
+  end 
 end
 
 def take_a_number(array, name)
