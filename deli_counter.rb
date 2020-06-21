ea03a9bbd6b5e2496546ab8c63fd8400e21ac55d
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-21 15:52:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ katz_deli = []
 
 def line(array)
 puts "The line is currently empty." if array.empty?
-array.join("")
+counter = 1 
+array.each do |name|
+nuarray.push("#{counter}. #{name}")
+counter += 1 
+end
 end
 
 def take_a_number(array, name)
