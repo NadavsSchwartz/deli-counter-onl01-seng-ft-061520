28ba5ccb1c86682056a138b9276d11b7d1baaf49
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-22 02:11:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(line, new_person)
 end
 
 def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
+  if line.length == 0 # could say: "if "
     puts"There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
